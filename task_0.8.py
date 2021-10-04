29cfe9ce07bad5d0a6575361101526b2af12cd2a
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2021-10-04 23:01:01 +0200

    Update task_0.8.py

--- task_0.8.py
+++ task_0.8.py
@@ -1,6 +1,20 @@
 num = int(input('Enter any number: '))
-#def time_converter(num):
-hours = num // 60
-minutes = (num) - (hours * 60)
-print(str(hours) + " Hour(s) and " + str(minutes) + " minute(s)" )
- #   return 
+def time_converter_hour(num):
+    hour = num // 60
+    return hour
+hour = time_converter_hour(num)
+if hour == 1 :
+    hour_string = "Hour"
+else:
+    hour_string = "Hours"
+    
+def time_converter_min(num):
+    minutes = num - (hour * 60)
+    return minutes
+minutes = time_converter_min(num)
+if minutes == 1:
+        minute_string = "Minute"
+else:
+        minute_string = "Minutes"
+        
+print("The number " + str(num),"is equal to " + str(hour) + " " + str(hour_string) + " and " + str(minutes) + " " + str(minute_string))
