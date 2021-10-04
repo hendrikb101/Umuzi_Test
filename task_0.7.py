51ba53ec02e87644e1e68371554bb3efc9661a1c
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2021-10-04 21:29:54 +0200

    Update task_0.7.py

--- task_0.7.py
+++ task_0.7.py
@@ -1,15 +1,15 @@
 c = int(input('Enter Celsius: '))
-def temp_converter(c):
+def temp_converter_fahrenheit(c):
     fahrenheit = (c * 9/5) + 32
     return fahrenheit
-fahrenheit = temp_converter(c)
+fahrenheit = temp_converter_fahrenheit(c)
 print(str(fahrenheit) + " Fahrenheit")
 
 
 
 f = int(input('Enter Fahrenheit: '))
-def temp_converter(f):
+def temp_converter_celsius(f):
     celsius = (f - 32) * 5/9
     return celsius
-celsius = temp_converter(f)
+celsius = temp_converter_celsius(f)
 print(str(celsius) + " Celsius")
