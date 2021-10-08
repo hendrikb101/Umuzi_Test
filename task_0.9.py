8417c659f0e446b8acc31e928ea34c64ef9303bd
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2021-10-08 10:29:31 +0200

    Update task_0.9.py

--- task_0.9.py
+++ task_0.9.py
@@ -1,5 +1,13 @@
-str_input = input('')
-vowels = [a,e,i,o,u]
-for car in vowels:
-    if str_input == vowels:
-        print(vowels)
+my_sentence = input(str("Please enter a string? "))
+def check_vowels(my_sentence):
+    my_list = ['a','A','e','E','o','O','i','I','u','U']
+    result = 0
+    for x in my_sentence:
+        return x
+    x = check_vowels(my_sentence)
+    print(x)
+    if x in my_list:
+       result = x + "," + (result)
+    print(str("Vowels in this string: " + result))
+
+    # I really do not know how to do this one, spend quite some time on it
