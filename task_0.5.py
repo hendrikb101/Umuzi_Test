16c4ba47fdc8cdf151abc042eb8a7d8101037ceb
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-01-18 17:09:11 +0200

    Delete task_0.5.py

--- task_0.5.py
+++
@@ -1,8 +0,0 @@
-x = int(input('Enter a number: '))
-y = int(input('Enter a number: '))
-z = int(input('Enter a number: '))
-def triangle_area(x, y, z):
-    total = (x + y + z) /2
-    return total
-total = triangle_area(x, y, z)
-print(str(total) + " is the total area size of the triangle")
