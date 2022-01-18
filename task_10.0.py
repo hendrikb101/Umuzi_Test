94d5dc8b81d678aac8337ec54266303e257b70a5
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-01-18 16:54:09 +0200

    Update task_10.0.py

--- task_10.0.py
+++ task_10.0.py
@@ -1,8 +1,11 @@
 word1 = str(input('Enter a word: '))
 word2 = str(input('Enter a second word: '))
 def compare(word1, word2):
-    for x in word1:
-        return x
-    x = compare(word1, word2)
-
-    ## Really struggling working with 2 inputs in a function, need more training
+    match = ""
+    for char1 in word1:
+        for char2 in word2:
+            if char2 == char1:
+                match = match + char2
+    return match
+match = compare(word1, word2)
+print(match)
