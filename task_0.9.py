853b83ba45f970008c93acb3769deb76fd11a42e
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-01-18 17:09:40 +0200

    Delete task_0.9.py

--- task_0.9.py
+++
@@ -1,9 +0,0 @@
-text = input('Please enter a string? ')
-def check_vowels(text):
-    tot = ""
-    for char in text:
-        if char in 'aeiouAEIOU':
-            tot = tot + char
-    return tot
-tot = check_vowels(text)
-print(tot)
