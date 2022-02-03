5a59119db360c1ab92c5dd0e924f1328956d66a8
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-03 23:37:47 +0200

    Update task_10.0.py

--- task_10.0.py
+++ task_10.0.py
@@ -1,11 +1,10 @@
-word1 = "house"
-word2 = "computers"
 def compare(word1, word2):
-    match = ""
+    match = []
     for char1 in word1:
         for char2 in word2:
-            if char2 == char1:
-                match = match + char2 + ", "
-    return match
-match = compare(word1, word2)
-print("Common Letters: " + match)
+            if char1.casefold() == char2.casefold():
+                match.append(char2)
+    match = ", ".join(match)
+    print("Common letters: " + match)
+
+compare("dEar","ear")
