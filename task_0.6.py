9b9b9100d1a38e54df28962ddce6fb08fda7122f
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-03 23:54:37 +0200

    Update task_0.6.py

--- task_0.6.py
+++ task_0.6.py
@@ -1,6 +1,3 @@
-a = 20
-b = 60
-c = 30
 def numbers(a, b, c):
     if a > b :
         max_num = a
@@ -8,6 +5,6 @@ def numbers(a, b, c):
         max_num = b
     elif c > a :
         max_num = c
-    return max_num
-max_num = numbers(a, b, c)
-print(str(max_num) + " is the highest number of the three")
+    print(str(max_num))
+
+numbers(12,20,30)
