2e39835b54ebff7f6ecab23de23547b36d5aec55
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-03 23:16:24 +0200

    Update task_0.9.py

--- task_0.9.py
+++ task_0.9.py
@@ -1,9 +1,14 @@
-text = "Umuzi"
 def check_vowels(text):
-    tot = ""
-    for char in text:
-        if char in 'aeiouAEIOU':
-            tot = tot + char + ", "
-    return tot
-tot = check_vowels(text)
-print("Vowels: " + tot)
+    vowels = ['a','e','i','o','u','A','E','I','O','U']
+    my_word = []
+    text_lower = set(text.lower())
+
+    for char in text_lower:
+        for vowel in vowels:
+            if char == vowel:
+                my_word.append(char)
+    my_word = ", ".join(my_word)
+    print("Vowels: " + my_word)
+
+
+check_vowels("Umuzi")
