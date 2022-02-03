c32381596ac4ecbca10b6ce08414749e4668453f
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-03 23:57:09 +0200

    Update task_0.5.py

--- task_0.5.py
+++ task_0.5.py
@@ -1,8 +1,5 @@
-x = 10
-y = 20
-z = 30
 def triangle_area(x, y, z):
     total = (x + y + z) /2
-    return total
-total = triangle_area(x, y, z)
-print(str(total) + " is the total area size of the triangle")
+    print(str(total))
+
+triangle_area(4,5,3)
