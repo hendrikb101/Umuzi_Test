8c599fb3ec8cd015b57f7ed12ee7bbb2143a8da5
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-15 15:29:23 +0200

    Update task_0.6.py

--- task_0.6.py
+++ task_0.6.py
@@ -1,10 +1,6 @@
-def numbers(a, b, c):
-    if a > b :
-        max_num = a
-    elif b > c:
-        max_num = b
-    elif c > a :
-        max_num = c
-    print(str(max_num))
-
-numbers(12,20,30)
+def maximum(a, *nums):
+    max_num = a
+    for x in nums:
+        if x > max_num:
+            max_num = x
+    return max_num
