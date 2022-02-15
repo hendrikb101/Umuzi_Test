d5ce8b7c664059afe68f25c8eac8ba0c19394990
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-15 15:28:27 +0200

    Update task_0.4.py

--- task_0.4.py
+++ task_0.4.py
@@ -3,8 +3,6 @@ def even_or_odd(num):
         num_result="even"
     else:
         num_result = "odd"
-    #return num_result
-    #num_result = even_or_odd(num)  
     print(num_result)
 
-even_or_odd(10)
+even_or_odd(4)
