4e95ab616e1746dd65b701c303f4a8f624d4c089
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-17 09:13:51 +0200

    Update task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -1,9 +1,6 @@
 def triangle_area(x,y,z):
-    s = (x + y + z) / 2
-    area = (s*(s-x)*(s-y)*(s-z)) ** 0.5
-#    print(area)
+    serimeter = (x + y + z) / 2
+    area = (serimeter*(serimeter-x)*(serimeter-y)*(serimeter-z)) ** 0.5
     return area
     
-print(triangle_area(6,5,5))
-
-
+print(triangle_area(12,13,14))
