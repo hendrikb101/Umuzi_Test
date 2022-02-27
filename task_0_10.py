9138287916c62fba172f636c161505d0fdd81b5c
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-27 21:48:23 +0200

    Update task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -1,15 +1,8 @@
 def compare(word1, word2):
-    match = []
-    for char1 in word1:
-        for char2 in word2:
-            if char1.casefold() == char2.casefold():
-                match.append(char2)
+    first_word = set(word1)
+    sec_word = set(word2)
     
-    new_match = []
-    for a in match:
-        if a not in new_match:
-            new_match.append(a)
-    new_match = ", ".join(new_match)
-    print(f"Common letters: {new_match}")
+    print("Common letters: " + ", ".join(sorted(first_word.intersection(sec_word))))
+    
 
 compare("look","fool")
