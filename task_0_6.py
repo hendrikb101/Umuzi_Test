963bab7e0dc131389d68795c1f5692b9e1d00933
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-28 22:03:29 +0200

    Update task_0_6.py

--- task_0_6.py
+++ task_0_6.py
@@ -1,8 +1,9 @@
-def maximum(*args):
-    max_num = max(args)
+def maximum(a, *nums):
+    max_num = a
+    for x in nums:
+        if x > max_num:
+            max_num = x
     return max_num
 
-# Task 0.6
-# Do this without using any builtin methods. Write your own logic from scratch.
 
-# This is what I've done, and wasn't good enough, so hopefully *args and max() is not "build in functions"
+print(maximum(0, -1, 1,4,9,80))
