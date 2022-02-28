9bab5c6d0be8f2718216b5ea9187611c70c32544
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-02-28 22:05:16 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -1,12 +1,12 @@
 def time_converter(num):
     hour = num // 60
-    if hour == 0 or 1 :
+    if hour ==  1 :
         hour_string = "hour"
     else:
         hour_string = "hours"
     
     minutes = num - (hour * 60)
-    if minutes == 0 or 1:
+    if minutes ==  1:
         minute_string = "minute"
     else:
         minute_string = "minutes"
