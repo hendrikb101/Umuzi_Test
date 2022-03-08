99ac77a982369d9b4c2fc9b41732b3e56a6f3680
hendrikb101 <36632702+hendrikb101@users.noreply.github.com>
2022-03-08 12:10:22 +0200

    Update task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -1,6 +1,6 @@
 def triangle_area(side_1,side_2,side_3):
-    serimeter = (side_1 + side_2 + side_3) / 2
-    area = (serimeter*(serimeter-side_1)*(serimeter-side_2)*(serimeter-side_3)) ** 0.5
+    semiperimeter = (side_1 + side_2 + side_3) / 2
+    area = (semiperimeter*(semiperimeter-side_1)*(semiperimeter-side_2)*(semiperimeter-side_3)) ** 0.5
     return area
     
 print(triangle_area(12,13,14))
